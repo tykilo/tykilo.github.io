fdbfa164eb7bb125399ba263dfad4a837acde10b
tykilo <15921328654@163.com>
2015-12-14 17:13:01 +0800

    CSS 样式

--- js/base.js
+++ js/base.js
@@ -1,21 +1,15 @@
+jQuery(document).ready(function($) {
 // preloader
+	$(window).load(function() {
+		$('#spinner').fadeOut(200);
+		$('#preloader').delay(200).fadeOut('slow');
+		$('.wrapper').fadeIn(200);
+		$('#custumize-style').fadeIn(200);
+	});
 
-$(window).load(function(){
-    $('.preloader').delay(1000).fadeOut("slow"); // set duration in brackets    
-});
-
-//home background slide
-$(function(){
-    jQuery(document).ready(function() {
-		$('body').backstretch([
-	 		 "images/tm-bg-slide-1.jpg", 
-	 		 "images/tm-bg-slide-2.jpg",
-			 "images/tm-bg-slide-3.jpg"
-	 			], 	{duration: 3200, fade: 1300});
-		});
-})
 
 // activate progress-bar 
-$(document).ready(function() {
-	$('.progress .progress-bar').progressbar({display_text: 'center'});
+	$(document).ready(function () {
+		$('.progress .progress-bar').progressbar({display_text: 'center'});
+	});
 });
