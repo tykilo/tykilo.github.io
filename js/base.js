661999337a008c7f2a7f7c7c51bfdbb6f4831bf9
tykilo <15921328654@163.com>
2015-12-14 22:59:53 +0800

    bg-pic

--- js/base.js
+++ js/base.js
@@ -1,25 +1,35 @@
-jQuery(document).ready(function($) {
-// preloader
-	$(window).load(function() {
-		$('#spinner').fadeOut(200);
-		$('#preloader').delay(200).fadeOut('slow');
-		$('.wrapper').fadeIn(200);
-		$('#custumize-style').fadeIn(200);
-	});
-
-
-// activate progress-bar 
+// activate progress-bar
 	$(document).ready(function () {
 		$('.progress .progress-bar').progressbar({display_text: 'center'});
 	});
+
+$(window).load(function(){
+	$('.preloader').delay(1000).fadeOut("slow"); // set duration in brackets
 });
 
+// HOME BACKGROUND SLIDESHOW
 $(function(){
 	jQuery(document).ready(function() {
 		$('body').backstretch([
-			"images/tm-bg-slide-1.jpg",
-			"images/tm-bg-slide-2.jpg",
-			"images/tm-bg-slide-3.jpg"
+
+			"images/2.jpg",
+
+			"images/4.jpg",
+
+			"images/6.jpg",
+
+			"images/bg2.jpg",
+			"images/bg3.jpg"
+
 		], 	{duration: 3200, fade: 1300});
 	});
-})
+});
+
+$(document).ready(function() {
+
+	$('#skill-progress .progress-bar').each(function () {
+		var $pb = $(this);
+		$pb.attr('data-transitiongoal', $pb.attr('data-transitiongoal-backup'));
+		$pb.progressbar({display_text: 'center'});
+	});
+});
