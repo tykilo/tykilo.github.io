01eaa27b8890a87217e3ecb8fc1541531a7a3729
tykilo <15921328654@163.com>
2015-12-14 19:38:05 +0800

    JS test1

--- js/base.js
+++ js/base.js
@@ -13,3 +13,13 @@ jQuery(document).ready(function($) {
 		$('.progress .progress-bar').progressbar({display_text: 'center'});
 	});
 });
+
+$(function(){
+	jQuery(document).ready(function() {
+		$('body').backstretch([
+			"images/tm-bg-slide-1.jpg",
+			"images/tm-bg-slide-2.jpg",
+			"images/tm-bg-slide-3.jpg"
+		], 	{duration: 3200, fade: 1300});
+	});
+})
